0a2c1b74f14a04a3bb099b19a14a5f9e9a5b0001
Márcio Macedo <marciocfmacedo@hotmail.com>
2022-05-08 14:49:52 -0300

    Validation layers have been configured

--- include/GraphicInstance.h
+++ include/GraphicInstance.h
@@ -3,6 +3,7 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 #include <vulkan/vulkan.hpp>
+#include <stdexcept>
 
 class GraphicInstance
 {
@@ -11,5 +12,16 @@ public:
 	~GraphicInstance();
 
 private:
+	bool checkValidationLayerSupport();
+	bool checkValidationLayerAvailability(const std::vector<const char*>& validationLayers, const std::vector<vk::LayerProperties>& availableLayers);
+	void createVulkanInstance(const std::string& applicationName);
+
+	const std::vector<const char*> validationLayers = { "VK_LAYER_KHRONOS_validation" };
+#ifdef NDEBUG
+	const bool enableValidationLayers = false;
+#else
+	const bool enableValidationLayers = true;
+#endif
 	vk::Instance vulkanInstance;
+
 };
--- src/GraphicInstance.cpp
+++ src/GraphicInstance.cpp
@@ -2,16 +2,57 @@
 
 GraphicInstance::GraphicInstance(const std::string& applicationName)
 {
-
-	uint32_t glfwExtensionCount = 0;
-	const char **glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-	const vk::ApplicationInfo applicationInfo{ applicationName.c_str(), 1, "No Engine", 1, VK_API_VERSION_1_0 };
-	const vk::InstanceCreateInfo createInfo{ {}, &applicationInfo, 0, nullptr, glfwExtensionCount, glfwExtensions};
-	vulkanInstance = vk::createInstance(createInfo);
-
+	if (enableValidationLayers && !checkValidationLayerSupport())
+	{
+		throw std::runtime_error("Validation layers requested, but not available!");
+	}
+	createVulkanInstance(applicationName);
 }
 
 GraphicInstance::~GraphicInstance()
 {
 	vulkanInstance.destroy();
+}
+
+bool GraphicInstance::checkValidationLayerSupport()
+{
+	const std::vector<vk::LayerProperties> availableLayers = vk::enumerateInstanceLayerProperties();
+	return checkValidationLayerAvailability(validationLayers, availableLayers);
+}
+
+bool GraphicInstance::checkValidationLayerAvailability(const std::vector<const char*>& validationLayers, const std::vector<vk::LayerProperties>& availableLayers)
+{
+	for (const char* layerName : validationLayers)
+	{
+		bool layerFound = false;
+		for (const auto& layerProperties : availableLayers)
+		{
+			if (strcmp(layerName, layerProperties.layerName) == 0)
+			{
+				layerFound = true;
+				break;
+			}
+		}
+		if (!layerFound)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+void GraphicInstance::createVulkanInstance(const std::string& applicationName)
+{
+
+	const vk::ApplicationInfo applicationInfo{ applicationName.c_str(), 1, "No Engine", 1, VK_API_VERSION_1_0 };
+	
+	uint32_t glfwExtensionCount = 0;
+	const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+	
+	uint32_t enabledLayerCount = (enableValidationLayers) ? static_cast<uint32_t>(validationLayers.size()) : 0;
+	const char* const* enabledLayerNames = (enableValidationLayers) ? validationLayers.data() : nullptr;
+	
+	const vk::InstanceCreateInfo createInfo{ {}, &applicationInfo, enabledLayerCount, enabledLayerNames, glfwExtensionCount, glfwExtensions };
+	vulkanInstance = vk::createInstance(createInfo);
+
 }
